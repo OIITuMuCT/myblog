5318163218be2822a2c0d25ef7f4094e5171e4d6
OIITuMuCT <vataman@live.ru>
2025-08-27 17:39:50 +0300

    Creating a nested Serializers

--- hello_world/backend/blog/serializers.py
+++ hello_world/backend/blog/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from blog.models import Blog
+from rest_framework import validators
+from blog.models import Blog, CoverImage, Tags
+from author.models import Author
+from 
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,11 +10,41 @@ class BlogSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class BlogCustomSerializer(serializers.ModelSerializer):
-    """ Custom Serializer """
+    """ Custom Create method """
     def create(self, validated_data):
         print('*** Custom Create method ***')
         return super(BlogCustomSerializer, self).create(validated_data)
 
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class BlogCustom2Serializer(serializers.ModelSerializer):
+    """ Custom update method """
+    def update(self, instance, validated_data):
+        print('*** Custom Update method')
+        return super(BlogCustom2Serializer, self).update(instance, validated_data)
+
+class BlogCustom3Serializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tags.objects.all(), many=True, allow_empty=True
+    )
+    cover_image = serializers.PrimaryKeyRelatedField(
+        queryset=CoverImage.objects.all(), validators=[validators.UniqueValidator(CoverImage.objects.all())]
+    )
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class BASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name', 'bio']
+
+class BlogCustom4Serializer(serializers.ModelSerializer):
+    author_details = BASerializer(source='author')
+    
     class Meta:
         model = Blog
         fields = '__all__'
