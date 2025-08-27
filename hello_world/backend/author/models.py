1e7522f939af705d935948daaea01e1b4aad4606
OIITuMuCT <vataman@live.ru>
2025-08-27 13:07:02 +0300

    Use fake migrations

--- hello_world/backend/author/models.py
+++ hello_world/backend/author/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    # demo_field = models.TextField(default='demo')
     email = models.EmailField()
     bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+class BlogAuthor(Author):
+    class Meta:
+        proxy = True
+
+    def perform_something(self):
+        pass
