d93f884599704a5ec9786419b14da0150704da69
OIITuMuCT <vataman@live.ru>
2025-08-27 20:02:13 +0300

    Using filter_horizontal

--- hello_world/backend/blog/admin.py
+++ hello_world/backend/blog/admin.py
@@ -16,9 +16,16 @@ class BlogCustomAdmin(admin.ModelAdmin):
 # admin.site.register(Blog, BlogCustomAdmin)
 
 class BlogCustom2Admin(admin.ModelAdmin):
+    """Adding custom fields"""
     list_display = ['title', 'word_count', 'id']
 
     def word_count(self, obj):
         return obj.content.split()
 
-admin.site.register(Blog, BlogCustom2Admin)
+# admin.site.register(Blog, BlogCustom2Admin)
+
+class BlogCustom3Admin(admin.ModelAdmin):
+    """Using filter_horizontal"""
+    filter_horizontal = ['tags']
+
+# admin.site.register(Blog, BlogCustom3Admin)
