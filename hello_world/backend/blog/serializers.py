b29fb5ebdb69aab087e6bcd82170f798d25828e3
OIITuMuCT <vataman@live.ru>
2025-08-27 16:57:21 +0300

    Creating a BlogCustomSerializer

--- hello_world/backend/blog/serializers.py
+++ hello_world/backend/blog/serializers.py
@@ -6,3 +6,12 @@ class BlogSerializer(serializers.ModelSerializer):
         model = Blog
         fields = '__all__'
 
+class BlogCustomSerializer(serializers.ModelSerializer):
+    """ Custom Serializer """
+    def create(self, validated_data):
+        print('*** Custom Create method ***')
+        return super(BlogCustomSerializer, self).create(validated_data)
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
