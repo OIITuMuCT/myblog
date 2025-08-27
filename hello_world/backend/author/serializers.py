9f2c6cd8f2aaedb1f59fc7e68f0b35371409daad
OIITuMuCT <vataman@live.ru>
2025-08-27 18:06:38 +0300

    Adding docstring

--- hello_world/backend/author/serializers.py
+++ hello_world/backend/author/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from author.models import Author
 
 class AuthorSerializer(serializers.ModelSerializer):
+    """Exploring source"""
     long_bio = serializers.CharField(source='bio')
     short_bio = serializers.CharField(source='fetch_short_bio')
 
