cce9717b4407dff08c250ca2d87f7e922d041ab4
OIITuMuCT <vataman@live.ru>
2025-08-27 12:48:03 +0300

    OneToOneField

--- hello_world/backend/blog/models.py
+++ hello_world/backend/blog/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
+    """ Blog model """
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey('author.Author', on_delete=models.CASCADE)
+    author = models.ForeignKey('author.Author',
+                            related_name='author_blogs',
+                            on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+class CoverImage(models.Model):
+    image_link = models.URLField()
+    blog = models.OneToOneField(Blog, related_name='blog_ci', on_delete=models.PROTECT)
