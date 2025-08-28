83e99a1b911a0cac0056272db5aa26a2a1ef98e5
OIITuMuCT <vataman@live.ru>
2025-08-28 17:10:37 +0300

    Adding a views blog_list, blog_view

--- hello_world/backend/blog/urls.py
+++ hello_world/backend/blog/urls.py
@@ -5,4 +5,6 @@ from blog import views
 urlpatterns = [
     path('hello-world/', views.basic_req),
     path('hello-world-2/', views.basic_req_2),
+    path('', views.BlogListView.as_view(), name='blog_list'),
+    path('view/', views.blog_view)
 ]
