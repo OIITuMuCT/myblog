60abcd20856964ef53807ed8ca4d5126f2761e7f
OIITuMuCT <vataman@live.ru>
2025-08-28 17:11:05 +0300

    Creating a BlogListView

--- hello_world/backend/blog/views.py
+++ hello_world/backend/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import HttpResponse
 from rest_framework import views
 from rest_framework import generics
 from rest_framework import filters
@@ -12,7 +14,8 @@ from blog.models import Blog
 
 
 class BlogGetCreateView(views.APIView):
-    """ List of Blogs """
+    """List of Blogs"""
+
     def get(self, request):
         blog_obj_list = Blog.objects.all()
         blogs = BlogSerializer(blog_obj_list, many=True)
@@ -24,7 +27,13 @@ class BlogGetCreateView(views.APIView):
         if b_obj.is_valid():
             b_obj.save()
             return Response(b_obj.data, status=status.HTTP_201_CREATED)
-        return Response(b_obj.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(b_obj.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = "blog_list.html"
+
 
 class BlogGetUpdateView(generics.ListCreateAPIView):
     serializer_class = BlogSerializer
@@ -33,19 +42,41 @@ class BlogGetUpdateView(generics.ListCreateAPIView):
         blogs_queryset = Blog.objects.filter(id__gt=1)
         return blogs_queryset
 
+
 class BlogGetUpdateFilterView(generics.ListAPIView):
     serializer_class = BlogSerializer
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['title']
+    ordering_fields = ["title"]
 
-@api_view(['GET'])
+
+def update_blog_title(request):
+    blog_id = request.GET.get("id")
+    blog = Blog.objects.get(id=blog_id)
+    if request.user.has_perm("blog.update_title"):
+        return HttpResponse("User has permission to update title")
+    return HttpResponse("User does not have permission to update title")
+
+
+def check_permission(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+
+@api_view(["POST"])
+def blog_view(request):
+    if not check_permission(request.user, "can_view_blog"):
+        return Response(status=403)
+    # perform operation
+
+
+@api_view(["GET"])
 def basic_req(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         resp = {"msg": "hello world!"}
         return Response(data=resp, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def basic_req_2(request):
-    if request.method == 'GET':
-        resp = {'msg': 'hello world!'}
+    if request.method == "GET":
+        resp = {"msg": "hello world!"}
         return Response(data=resp, status=status.HTTP_200_OK)
